fbbd95ea7ef2b832287798683b44ba0fd3fe37f6
masa-laughndie <masaya.oor710@gmail.com>
2020-01-26 12:09:42 +0900

    extract code generator from 9cc.c to codegen.c

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -1,60 +1,5 @@
 #include "9cc.h"
 
-//
-// Code generator
-//
-
-void gen(Node *node) {
-  if (node->kind == ND_NUM) {
-    printf("  push %d\n", node->val);
-    return;
-  }
-
-  gen(node->lhs);
-  gen(node->rhs);
-
-  printf("  pop rdi\n");
-  printf("  pop rax\n");
-
-  switch (node->kind) {
-  case ND_ADD:
-    printf("  add rax, rdi\n");
-    break;
-  case ND_SUB:
-    printf("  sub rax, rdi\n");
-    break;
-  case ND_MUL:
-    printf("  imul rax, rdi\n");
-    break;
-  case ND_DIV:
-    printf("  cqo\n");
-    printf("  idiv rdi\n");
-    break;
-  case ND_EQ:
-    printf("  cmp rax, rdi\n");
-    printf("  sete al\n");
-    printf("  movzb rax, al\n");
-    break;
-  case ND_NE:
-    printf("  cmp rax, rdi\n");
-    printf("  setne al\n");
-    printf("  movzb rax, al\n");
-    break;
-  case ND_LT:
-    printf("  cmp rax, rdi\n");
-    printf("  setl al\n");
-    printf("  movzb rax, al\n");
-    break;
-  case ND_LE:
-    printf("  cmp rax, rdi\n");
-    printf("  setle al\n");
-    printf("  movzb rax, al\n");
-    break;
-  }
-
-  printf("  push rax\n");
-}
-
 int main(int argc, char **argv) {
   if (argc != 2) {
     error("%s: Wrong number of arguments!", argv[0]);
@@ -66,13 +11,7 @@ int main(int argc, char **argv) {
   token = tokenize();
   Node *node = expr();
 
-  printf(".intel_syntax noprefix\n");
-  printf(".global main\n");
-  printf("main:\n");
-
-  gen(node);
-
-  printf("  pop rax\n");
-  printf("  ret\n");
+  // Travers the AST to emit assembly
+  codegen(node);
   return 0;
 }
--- 9cc/9cc.h
+++ 9cc/9cc.h
@@ -67,4 +67,4 @@ Node *expr();
 // codegen.c
 //
 
-void gen(Node *node)
+void codegen(Node *node);
